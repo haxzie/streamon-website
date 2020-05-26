a3f8385c5fe49a081f8e5d6edbae6ebaf5519a82
haxzie <musthu.gm@gmail.com>
2020-05-26 11:24:02 +0530

    Add beta releases

--- src/components/DownloadSection/index.jsx
+++ src/components/DownloadSection/index.jsx
@@ -5,12 +5,21 @@ import LinuxLogo from "../../images/linux-logo.png"
 import WindowsLogo from "../../images/windows-logo.png"
 import AppleLogo from "../../images/apple-logo.png"
 
-export default function DownloadSection() {
+export default function DownloadSection({
+  title,
+  version,
+  releaseDate,
+  windows,
+  linux,
+  macos,
+}) {
   return (
     <section className={styles.downloadSection}>
       <Container>
-        <h1 className={styles.pageTitle}>Downloads</h1>
-        <p className={styles.pageDescription}>Latest Version: v0.1.1 Beta [04/05/2020]</p>
+        <h1 className={styles.pageTitle}>{title}</h1>
+        <p
+          className={styles.pageDescription}
+        >{`Latest Version: ${version} [${releaseDate}]`}</p>
 
         <div className={styles.platformGrid}>
           <div className={styles.platformCard}>
@@ -19,8 +28,14 @@ export default function DownloadSection() {
               <h4 className={styles.title}>Linux</h4>
             </header>
             <ul className={styles.downloadsList}>
-                <li><h4>deb</h4><a target="_blank" rel="noopener" href="https://drive.google.com/open?id=1kAEQESBHLW4ZrO0vKUc1X4dT7Wst9UpH">Download</a></li>
-                <li><h4>AppImage</h4><a target="_blank" rel="noopener" href="https://drive.google.com/open?id=1iTLzFHm37Sxf33Xe97qVOsE42_0UsGIP">Download</a></li>
+              {linux.map(item => (
+                <li>
+                  <h4>{item.type}</h4>
+                  <a target="_blank" rel="noopener" href={item.link}>
+                    Download
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={styles.platformCard}>
@@ -29,18 +44,40 @@ export default function DownloadSection() {
               <h4 className={styles.title}>Windows</h4>
             </header>
             <ul className={styles.downloadsList}>
-                <li><h4>exe</h4><a target="_blank" rel="noopener" href="https://drive.google.com/open?id=1V6w05GB9wX1YIAWF8efm7yyN6q6kgLAA">Download</a></li>
+              {windows.map(item => (
+                <li>
+                  <h4>{item.type}</h4>
+                  <a target="_blank" rel="noopener" href={item.link}>
+                    Download
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
-          <div className={styles.platformCard}>
-            <header>
-              <img className={styles.platformIcon} src={AppleLogo} alt="" />
-              <h4 className={styles.title}>Mac Os</h4>
-            </header>
-            <ul className={styles.downloadsList}>
-                <li><h4>Coming soon...</h4></li>
-            </ul>
-          </div>
+          {macos ? (
+            <div className={styles.platformCard}>
+              <header>
+                <img className={styles.platformIcon} src={AppleLogo} alt="" />
+                <h4 className={styles.title}>Mac Os</h4>
+              </header>
+              <ul className={styles.downloadsList}>
+                {macos.map(item => (
+                  <li>
+                    <h4>{item.type}</h4>
+                    {item.link ? (
+                      <a target="_blank" rel="noopener" href={item.link}>
+                        Download
+                      </a>
+                    ) : (
+                      <></>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </Container>
     </section>
--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -5,8 +5,61 @@ import DownloadsSection from "../components/DownloadSection"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Downloads" />  
-    <DownloadsSection/>
+    <SEO title="Downloads" />
+    <DownloadsSection
+      title="Downloads [Stable]"
+      version="v0.1.1"
+      releaseDate="04/05/2020"
+      linux={[
+        {
+          type: "deb",
+          link:
+            "https://drive.google.com/open?id=1kAEQESBHLW4ZrO0vKUc1X4dT7Wst9UpH",
+        },
+        {
+          type: "AppImage",
+          link:
+            "https://drive.google.com/open?id=1iTLzFHm37Sxf33Xe97qVOsE42_0UsGIP",
+        },
+      ]}
+      windows={[
+        {
+          type: "exe",
+          link:
+            "https://drive.google.com/open?id=1V6w05GB9wX1YIAWF8efm7yyN6q6kgLAA",
+        },
+      ]}
+      macos={[
+        {
+          type: "coming soon...",
+          link: null,
+        },
+      ]}
+    />
+    <DownloadsSection
+      title="Beta Channel"
+      version="v0.1.2-beta"
+      releaseDate="26/05/2020"
+      linux={[
+        {
+          type: "deb",
+          link:
+            "https://github.com/haxzie/instagram-live-streamer/releases/download/0.1.2-beta/streamon_0.1.2_amd64.deb",
+        },
+        {
+          type: "AppImage",
+          link:
+            "https://github.com/haxzie/instagram-live-streamer/releases/download/0.1.2-beta/streamon-0.1.2.AppImage",
+        },
+      ]}
+      windows={[
+        {
+          type: "exe",
+          link:
+            "https://github.com/haxzie/instagram-live-streamer/releases/download/0.1.2-beta/streamon.Setup.0.1.2.exe",
+        },
+      ]}
+    />
   </Layout>
 )
 
