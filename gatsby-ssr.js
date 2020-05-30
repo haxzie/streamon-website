63d4210f951dfee090d86ee3ed9d6cf012fdd317
haxzie <musthu.gm@gmail.com>
2020-05-30 15:48:07 +0530

    Add BMC link

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require("react")
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      data-name="BMC-Widget"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="haxzie"
+      data-description="Support me on Buy me a coffee!"
+      data-message="Help us keep this project alive. "
+      data-color="#5F7FFF"
+      data-position="right"
+      data-x_margin="18"
+      data-y_margin="18"
+    ></script>,
+  ])
+}
