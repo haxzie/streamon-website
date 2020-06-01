86b309d0c5774c71487dfc73cc03132a02de1276
haxzie <musthu.gm@gmail.com>
2020-06-01 10:01:02 +0530

    Add drop down menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./styles.module.scss"
 import Container from "../Container"
 import LogoRef from "../../images/logo.svg"
 import { Link } from "gatsby"
+import MenuIcon from "../../images/open-menu.svg"
+import CloseIcon from "../../images/close.svg"
+
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <header className={styles.header}>
       <Container>
@@ -53,9 +57,49 @@ export default function Header() {
           </ul>
 
           {/* Mobile navigation dropdown */}
-          {/* <div className={styles.mobileDropDown}>
-            
-          </div> */}
+          <div className={styles.mobileDropDown}>
+           { !showMenu? <img src={MenuIcon} className={styles.menuToggle} onClick={() => setShowMenu(!showMenu)}/>:
+            <img src={CloseIcon} className={styles.menuToggle} onClick={() => setShowMenu(!showMenu)}/>}
+            {showMenu ? (
+              <ul className={styles.menuOptions}>
+                <li>
+                  <Link to="/downloads">Downloads</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href="https://github.com/haxzie/instagram-live-streamer/releases"
+                  >
+                    Changelog
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href="https://github.com/haxzie/instagram-live-streamer"
+                  >
+                    GitHub
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href="https://www.buymeacoffee.com/haxzie"
+                  >
+                    Buy me a coffee
+                  </a>
+                </li>
+              </ul>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </Container>
     </header>
